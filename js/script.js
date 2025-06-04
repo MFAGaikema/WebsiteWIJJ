68705c5eca36abab3333fd99ef40b7e9affb08db
Marijn Gaikema <marijngaikema@gmail.com>
2025-06-04 13:58:59 +0200

    Restructured scss files and added styling for desktop

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-//Navbar elements
-const navbarToggleBtn = document.querySelector('#navbar-toggle-btn');
-const toggleBtnIcon = navbarToggleBtn.querySelector('i');
-const headerLogoLink = document.querySelector('.header-logo');
-const mobileTabletNavMenu = document.getElementById('mobile-tablet-navigation');
+//Elements
 const header = document.querySelector('header');
+const headerLogoLink = document.querySelector('.header-logo');
+const navbarToggleBtn = document.querySelector('.navbar-toggle-btn');
+const toggleBtnIcon = navbarToggleBtn.querySelector('i');
+const mobileTabletNavMenu = document.getElementById('mobile-tablet-navbar');
+const coachenLink = document.querySelector('.dropdown-link-coachen');
+const overonsLink = document.querySelector('.dropdown-link-overons');
 const coachenBtn = document.querySelector('.coachen-btn');
 const overonsBtn = document.querySelector('.overons-btn');
 const chevrons = Array.from(document.getElementsByClassName('fa-chevron-down'));
@@ -11,33 +13,46 @@ const dropdownContainers = Array.from(
 	document.getElementsByClassName('dropdown-container')
 );
 
-//ATTRIBUTEN DIE TOEGEVOEGD MOETEN WORDEN BIJ GROTERE BEELDSCHERMEN (OP DROPDOWNKNOP)
-// aria-haspopup="true"
-// 							aria-expanded="false"
-// 							aria-controls="coachen-menu"
-// aria-haspopup="true"
-// 							aria-expanded="false"
-// 							aria-controls="overons-menu"
-
 //Visibility of dropdowncontainer based on screenwidth
-const dropdownContainerUpdateAttributes = () => {
-	dropdownContainers.forEach((container) => {
-		if (window.innerWidth > 1024) {
-			container.setAttribute('hidden', true);
-		}
-	});
-};
+// const dropdownContainerUpdateAttributes = () => {
+// 	dropdownContainers.forEach((container) => {
+// 		if (window.innerWidth > 1024) {
+// 			container.setAttribute('hidden', true);
+// 		}
+// 	});
+// };
 
-window.addEventListener('resize', dropdownContainerUpdateAttributes);
-window.addEventListener('load', dropdownContainerUpdateAttributes);
+// window.addEventListener('resize', dropdownContainerUpdateAttributes);
+// window.addEventListener('load', dropdownContainerUpdateAttributes);
 
-//check of menu uitgeklapt is
+//check if menu is expanded
 const menuIsExpanded = (button) => {
 	return button.getAttribute('aria-expanded') === 'true';
 };
 
-//openen en sluiten van menu's algemeen
-const toggleAttributeHidden = (menuName, button, expanded) => {
+//change aria-label based on button class
+const setAriaLabel = (button, expanded) => {
+	if (button.classList[1] === 'coachen-btn') {
+		expanded
+			? button.setAttribute('aria-label', 'Coachen menu uitklappen')
+			: button.setAttribute('aria-label', 'Coachen menu inklappen');
+	}
+
+	if (button.classList[1] === 'overons-btn') {
+		expanded
+			? button.setAttribute('aria-label', 'Over ons menu uitklappen')
+			: button.setAttribute('aria-label', 'Over ons menu inklappen');
+	}
+
+	if (button.classList[0] === 'navbar-toggle-btn') {
+		expanded
+			? button.setAttribute('aria-label', 'Open navigatie')
+			: button.setAttribute('aria-label', 'Sluit navigatie');
+	}
+};
+
+//set attributes of dropdown buttons and menus on toggle
+const toggleDropdownAttributes = (menuName, button, expanded) => {
 	const menu = document.getElementById(menuName);
 	button.setAttribute('aria-expanded', !expanded);
 
@@ -48,25 +63,25 @@ const toggleAttributeHidden = (menuName, button, expanded) => {
 	} else {
 		menu.hidden = expanded;
 	}
+
+	setAriaLabel(button, expanded);
 };
 
-//openen en sluiten van navigatie mobile
+//open and close mobile navbar
 const toggleNavbar = () => {
-	//const expanded = navbarToggleBtn.getAttribute('aria-expanded') === 'true';
-	toggleAttributeHidden(
-		'mobile-tablet-navigation',
+	toggleDropdownAttributes(
+		'mobile-tablet-navbar',
 		navbarToggleBtn,
 		menuIsExpanded(navbarToggleBtn)
 	);
 
+	//toggle hamburger/close btn and move navigation in/out of screen
 	if (menuIsExpanded(navbarToggleBtn)) {
-		navbarToggleBtn.setAttribute('aria-label', 'Sluit navigatie');
 		toggleBtnIcon.setAttribute('class', 'fa-solid fa-xmark fa-2xl');
 		setTimeout(() => {
 			mobileTabletNavMenu.style.left = '0';
 		}, 1);
 	} else {
-		navbarToggleBtn.setAttribute('aria-label', 'Open navigatie');
 		toggleBtnIcon.setAttribute('class', 'fa-solid fa-bars fa-2xl');
 		mobileTabletNavMenu.style.left = '-100vw';
 	}
@@ -81,16 +96,16 @@ navbarToggleBtn.addEventListener('click', toggleNavbar);
 //};
 
 //Sluiten van navigatie als er buiten de header en navigatie wordt geklikt
-document.addEventListener('click', (e) => {
-	const isClickOnOpenBtn = navbarToggleBtn.contains(e.target);
-	const isClickOnHeader = header.contains(e.target);
+// document.addEventListener('click', (e) => {
+// 	const isClickOnOpenBtn = navbarToggleBtn.contains(e.target);
+// 	const isClickOnHeader = header.contains(e.target);
 
-	if (!isClickOnHeader && !isClickOnOpenBtn) {
-		navMenuMobile.setAttribute('hidden', '');
-		navbarToggleBtn.setAttribute('aria-label', 'Open navigatie');
-		toggleBtnIcon.setAttribute('class', 'fa-solid fa-bars fa-2xl');
-	}
-});
+// 	if (!isClickOnHeader && !isClickOnOpenBtn) {
+// 		//navMenuMobile.setAttribute('hidden', '');
+// 		navbarToggleBtn.setAttribute('aria-label', 'Open navigatie');
+// 		toggleBtnIcon.setAttribute('class', 'fa-solid fa-bars fa-2xl');
+// 	}
+// });
 
 //Voorkomen dat de focus uit de header gaat als de navigatie geopend is
 const focusableItems = Array.from(
@@ -122,9 +137,14 @@ header.addEventListener('keydown', (e) => {
 //Scroll behavior of header
 let initialHeaderPosition = window.pageYOffset;
 
-const handleHeader = () => {
+const handleScrollingHeader = () => {
 	const currentHeaderPosition = window.pageYOffset;
-	if (!menuIsExpanded(navbarToggleBtn)) {
+
+	if (
+		!menuIsExpanded(navbarToggleBtn) &&
+		!menuIsExpanded(coachenBtn) &&
+		!menuIsExpanded(overonsBtn)
+	) {
 		initialHeaderPosition > currentHeaderPosition
 			? (header.style.top = '0')
 			: (header.style.top = '-5em');
@@ -132,37 +152,72 @@ const handleHeader = () => {
 	}
 };
 
-window.addEventListener('scroll', handleHeader);
+window.addEventListener('scroll', handleScrollingHeader);
 
-const handleDropdownMenu = (button, menuName) => {
-	const expanded = button.getAttribute('aria-expanded') === 'true';
+//Dropdown behaviour navigation menu desktop
 
-	toggleAttributeHidden(menuName, button, expanded);
+//Close dropdown menu when other is opened, also reset chevron back to down
+const closeDropdown = (button, menuName) => {
+	const menu = document.getElementById(menuName);
+	menu.hidden = true;
 
-	const buttonClass = button.classList[0];
+	if (button.classList[1] === 'coachen-btn') {
+		overonsBtn.setAttribute('aria-label', 'Over ons menu uitklappen');
+		overonsBtn.querySelector('.fa-chevron-down').style.transform =
+			'rotate(0deg)';
+	}
+
+	if (button.classList[1] === 'overons-btn') {
+		overonsBtn.setAttribute('aria-label', 'Coachen menu uitklappen');
+		overonsBtn.querySelector('.fa-chevron-down').style.transform =
+			'rotate(0deg)';
+	}
+};
+
+const toggleDropdownMenu = (button, menuName) => {
+	toggleDropdownAttributes(menuName, button, menuIsExpanded(button));
+
+	const buttonClass = button.classList[1];
 
 	const chevron = document.querySelector(`.${buttonClass} .fa-chevron-down`);
 
-	expanded
-		? (chevron.style.transform = 'rotate(0deg)')
-		: (chevron.style.transform = 'rotate(180deg)');
+	menuIsExpanded(button)
+		? (chevron.style.transform = 'rotate(180deg)')
+		: (chevron.style.transform = 'rotate(0deg)');
 };
 
-//openen dropdowns
-coachenBtn.addEventListener('click', () =>
-	handleDropdownMenu(coachenBtn, 'coachen-menu')
-);
+//open dropdown menus
+coachenBtn.addEventListener('click', () => {
+	toggleDropdownMenu(coachenBtn, 'coachen-menu');
+	//closeDropdown(overonsBtn, 'overons-menu');
+});
+
 coachenBtn.addEventListener('keypress', (e) => {
 	if (e.key === 'Enter') {
-		handleDropdownMenu(coachenBtn, 'coachen-menu');
+		e.preventDefault();
+		toggleDropdownMenu(coachenBtn, 'coachen-menu');
+		//closeDropdown(overonsBtn, 'overons-menu');
 	}
 });
 
-overonsBtn.addEventListener('click', () =>
-	handleDropdownMenu(overonsBtn, 'overons-menu')
-);
+// coachenLink.addEventListener('mouseover', () => {
+// 	toggleDropdownMenu(coachenBtn, 'coachen-menu');
+// 	closeDropdown(overonsBtn, 'overons-menu');
+// });
+
+overonsBtn.addEventListener('click', () => {
+	toggleDropdownMenu(overonsBtn, 'overons-menu');
+	//closeDropdown(coachenBtn, 'coachen-menu');
+});
 overonsBtn.addEventListener('keypress', (e) => {
 	if (e.key === 'Enter') {
-		handleDropdownMenu(overonsBtn, 'coachen-menu');
+		e.preventDefault();
+		toggleDropdownMenu(overonsBtn, 'overons-menu');
+		//closeDropdown(coachenBtn, 'coachen-menu');
 	}
 });
+
+// overonsLink.addEventListener('mouseover', () => {
+// 	toggleDropdownMenu(overonsBtn, 'overons-menu');
+// 	closeDropdown(coachenBtn, 'coachen-menu');
+// });
